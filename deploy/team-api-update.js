b89cfe7fb05802d07ee52181dd45f79cd4a16669
Mike Bland <michael.bland@gsa.gov>
2015-09-04 17:14:09 -0400

    Fix team-api-update.js; rm extra function names
    The original promise function was returning/resolving early, causing the
    `finish` callback to fire before the actual bundle/jekyll calls.

--- deploy/team-api-update.js
+++ deploy/team-api-update.js
@@ -19,7 +19,7 @@ function TeamApiUpdater(repoDir) {
   this.repoDir = repoDir;
 }
 
-TeamApiUpdater.prototype.spawn = function spawn(actionDescription, path, args) {
+TeamApiUpdater.prototype.spawn = function (actionDescription, path, args) {
   var that = this;
   return new Promise(function(resolve, reject) {
     var spawnOpts = { cwd: that.repoDir, stdio: 'inherit' };
@@ -34,11 +34,11 @@ TeamApiUpdater.prototype.spawn = function spawn(actionDescription, path, args) {
   });
 };
 
-TeamApiUpdater.prototype.bundleInstall = function bundleInstall() {
+TeamApiUpdater.prototype.bundleInstall = function () {
   return this.spawn('bundle install', config.bundler, ['install']);
 };
 
-TeamApiUpdater.prototype.jekyllBuild = function jekyllBuild() {
+TeamApiUpdater.prototype.jekyllBuild = function () {
   return this.spawn('jekyll build', config.bundler,
     ['exec', 'jekyll', 'build', '--trace']);
 };
@@ -56,16 +56,21 @@ function finish(err) {
   }
 }
 
+function createUpdaterPromise(repoDir) {
+  var updater = new TeamApiUpdater(repoDir);
+  return updater.bundleInstall()
+    .then(function() { return updater.jekyllBuild(); });
+}
+
 webhook.on('refs/heads/' + config.teamApiBranch, function(info) {
   if (!isValidUpdate(info)) {
     return;
   }
 
-  var updatePromise = new Promise(function(resolve) { resolve(); });
+  var updatePromise = null;
+
   config.repoDirs.map(function (repoDir) {
-    var updater = new TeamApiUpdater(config.repoDir);
-    updatePromise.then(function() { return updater.bundleInstall(); })
-      .then(function() { return updater.jekyllBuild(); });
+    updatePromise = createUpdaterPromise(repoDir);
   });
 
   updatePromise.then(finish, finish);
