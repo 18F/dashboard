15d3d3e3565b1e55e99642b67beb4969535c4a84
Jeremia Kimelman <jeremia.kimelman@gsa.gov>
2015-12-03 15:38:09 -0500

    Use yamlJS as browser compatible YAML parser
    I wanted to test another yaml parser because `js-yaml` does not work
    well with browserify. `yamljs` works just fine. I updated all
    references to it, and tried to override the global jsYaml object so
    that you can easily comment out that dependency within the source code
    of the validator module and then the validator can consume this module.
    
    This is a stopgap. We need the validator module to make this change
    upstream.

--- assets/js/preview.js
+++ assets/js/preview.js
@@ -1,9 +1,15 @@
-// var aboutYmlValidator = require('about-yml-npm');
-// var validator = new aboutYmlValidator();
+var yaml = require('yamljs');
+
+// override global objects for validator
+window.jsYaml = window.jsyaml = {};
+window.jsYaml.safeLoad = yaml.parse;
+
+var aboutYmlValidator = require('about-yml-npm');
+var validator = new aboutYmlValidator();
 
 var fs = require('fs');
 
-var liquid = window.l = require('liquid.js');
+var liquid = window.l = modifyLiquidIncludes(require('liquid.js'));
 
 var url = cleanUrl(window.location.search);
 var xhr = new XMLHttpRequest();
@@ -16,11 +22,13 @@ function insertAboutYml (e) {
   var res = e.target;
   if (res.status !== 200) return;
 
-  var yml = jsyaml.safeLoad(res.responseText);
+  var yml = yaml.parse(res.responseText);
   var template = compileLiquidTemplate();
   var html = template.render({ page: { project: yml }});
-
   document.querySelector('[role=main]').innerHTML = html;
+
+  var validationErrors = validator.validate(res.responseText);
+  console.log('validationErrors', validationErrors);
 }
 
 function cleanUrl (url) {
@@ -30,7 +38,34 @@ function cleanUrl (url) {
 }
 
 function compileLiquidTemplate () {
-  var templateSrc = fs.readFileSync(__dirname + '/../../_layouts/project.html').toString();
+  var templateSrc = fs.readFileSync(__dirname + '/../../_layouts/project.html', 'utf8');
   templateSrc = templateSrc.replace(/---[\s\S]*---/, '');
+  templateSrc = templateSrc.replace(/include (.*?) /g, "include '$1' ");
+
   return liquid.parse(templateSrc);
+}
+
+function modifyLiquidIncludes (liquid) {
+  liquid.readTemplateFile = function(path) {
+    console.log('path', path);
+    var elem = $(path);
+    if(elem) {
+      return elem.innerHTML;
+    } else {
+      return path +" can't be found.";
+      // Or throw and error, or whatever you want...
+    }
+  }
+
+  return liquid;
+}
+
+function getIncludeFiles () {
+  var includes = {};
+  var basePath = __dirname + '/../../_includes';
+  console.log('basePath', basePath);
+  var files = fs.readFileSync(basePath);
+  // console.log('files', files);
+  console.log('includes', includes);
+
 }
