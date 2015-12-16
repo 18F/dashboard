2f5caa9eefe80f669c9fdeabf1e8c73b0ad25e93
Greg Boone <gregory.boone@gsa.gov>
2015-12-16 08:49:40 -0600

    Fixes the test to find the right number of expected pages

--- tests/test.rb
+++ tests/test.rb
@@ -1,9 +1,9 @@
 #! /usr/bin/env ruby
 require 'safe_yaml'
 
-data = SafeYAML.load_file '_data/project_filter.yml', safe: true
+data = YAML.safe_load_file '_data/project_filter.yml'
 redirects = Dir.glob('pages/project/*')
-expect_size = data.size + redirects.size
+expect_size = data['projects'].size + redirects.size
 actual_size = Dir.glob('_site/dashboard/project/*').size
 if expect_size == actual_size
   puts "Dashboard pages generated correctly. There are #{actual_size} projects."
